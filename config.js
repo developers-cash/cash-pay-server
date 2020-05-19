a321696121a079d5c5262e3fb5935dc24d35bd19
James Zuccon <zuccon@gmail.com>
2020-05-19 13:21:35 +1000

    Configuration changes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: 'dev',
   domain: process.env.DOMAIN,
-  certPath: '/etc/ssl/private',
-  wif: process.env.WIF || 'L4vmKsStbQaCvaKPnCzdRArZgdAxTqVx8vjMGLW5nHtWdRguiRi1'
+  certPath: process.env.CERT_PATH,
+  wif: process.env.WIF
 }
